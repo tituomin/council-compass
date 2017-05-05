3d876077986849c7df36ab93b46f00c42a4a538b
Timo Tuominen <tituomin@gmail.com>
2017-05-05 18:52:31 +0300

    per sent

--- src/components/mock/PartyMatchViz.js
+++ src/components/mock/PartyMatchViz.js
@@ -8,6 +8,6 @@ export default function PartyMatchViz({partyAbbr, partyFit, partyLogo}) {
   return (<div className="dib mb3">
             <a className="link dim white dib mh2 pv3 w2 h2 tc lh-copy cover bg-center" href="#" title={partyAbbr} style={indicatorStyle}>
             </a>
-            <div className="tc b gray">{partyFit}</div>
+            <div className="tc b gray">{partyFit + '%'}</div>
           </div>);
 }
