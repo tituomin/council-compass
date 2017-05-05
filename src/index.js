8f24424c98ee18490aa33ea01ad41c742f54fc08
Timo Tuominen <tituomin@gmail.com>
2017-05-05 15:55:19 +0300

    Remove redux cruft.

--- src/index.js
+++ src/index.js
@@ -2,25 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 import { castUserVote } from './actions';
 
 import { HashRouter as Router } from 'react-router-dom';
 
-import rootReducer from './reducers';
-
-const store = createStore(rootReducer);
-
 window.castUserVote = castUserVote;
-window.store = store;
 
 ReactDOM.render(
-  <Provider store={store}>
-      <Router>
-          <App/>
-      </Router>
-  </Provider>,
+  <Router>
+      <App/>
+  </Router>,
   document.getElementById('root'));
 
