7fa8ee4bb425f18b5771562e30bd027f0a622ff6
Tero Tikkanen <tero.tikkanen@gmail.com>
2017-05-05 16:23:09 +0300

    Update front page and navbar

--- src/components/mock/AppNavigation.js
+++ src/components/mock/AppNavigation.js
@@ -4,7 +4,7 @@ export default function AppNavigation() {
   return (<header className="fixed w-100 z-max">
           <div className="header-content bg-black-90 ph1 pv3 ph4 mw7 center">
             <nav className="f7 fw6 ttu tracked">
-              <a className="link dim light-green dib mr3 ba ph2 pv1" href="#" title="Home">Edustamo</a>
+              <a className="link dim light-green dib mr3 ba ph2 pv1" href="#" title="Home">Partisipaattori</a>
               <a className="link dim white dib mr3" href="#/motion/" title="Home">Äänestykset</a>
               <a className="link dim white dib mr3" href="#/party/" title="Home">Puolueet</a>
             </nav>
--- src/components/mock/FrontPage.js
+++ src/components/mock/FrontPage.js
@@ -5,14 +5,15 @@ import {Link} from 'react-router-dom';
 export default function FrontPage({nextVote}) {
   let caseLink = null;
   if (nextVote) {
-    caseLink = <Link to={`/motion/${nextVote}`} className="f6 link dim br-pill ph3 pv2 mb2 dib white bg-dark-pink mv4" href="#0">Aloitetaan äänestäminen!</Link>;
+    caseLink = <Link to={`/motion/${nextVote}`} className="f4 b link dim br-pill ph4 pv3 mb2 dib white bg-dark-pink mv4" href="#0">Aloitetaan äänestäminen!</Link>;
   }
   return (<div>
             <article className="center mw7 hidden ba b--light-gray mv4">
                 <div className="pa3 bg-white">
-                  <h1 className="f2">Palvelun nimi</h1>
-                  <h2 className="f3 gray">Iconic delightful Asia-Pacific Melbourne premium impeccable.</h2>
-                  <p className="lh-copy">Monocle ipsum dolor sit amet airport artisanal remarkable international the best Scandinavian. Toto finest sophisticated the best, Boeing 787 delightful cosy tote bag Asia-Pacific uniforms concierge. Extraordinary Winkreative global quality of life artisanal. Vibrant Baggu Boeing 787 alluring signature. Carefully curated izakaya hub elegant. Iconic delightful Asia-Pacific Melbourne premium impeccable. Helsinki charming Boeing 787 Scandinavian, Tsutaya the highest quality global concierge impeccable perfect.</p>
+                  <h1 className="f2 tc ttu tracked">Partisipaattori</h1>
+                  <hr className="mw3 bb bw1 b--black-10" />
+                  <h2 className="f3 gray tc">Tarkastele ja kerro mielipiteesi kaupunginvaltuuston tehdyistä päätöksistä.</h2>
+                  <p className="lh-copy tc">Partisipaattorin avulla voit tarkastella kaupunginvaltuustossa käsiteltyjä asioita ja tarkastella, miten niistä on äänestetty ehdokkaiden, puolueiden ja kaupunkilaisten toimesta. Saat itse kysymystä äänestettyäsi näkyviin omaa kantaasi parhaiten vastaavat edustajat kustakin asiasta. </p>
                   <p className="tc">
                     { caseLink }
                   </p>
