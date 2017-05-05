6a4a28eef10fdb96b9ffc93da42ac92030fd4c0c
Timo Tuominen <tituomin@gmail.com>
2017-05-05 14:03:38 +0300

    Add plenty of link usability to motion card.

--- src/components/mock/MotionCard.js
+++ src/components/mock/MotionCard.js
@@ -16,7 +16,7 @@ function voteValueDescription(value) {
 export default function MotionCard({_case, totalCount, userVote}) {
   const userVoteSummary = (userVote !== undefined ? 'Äänestit ' + voteValueDescription(userVote).toUpperCase() : 'Äänestä');
   return (<article className="ba mb3 b--black-10 bg-white shadow-4">
-              <a className="db pv3 ph3 no-underline black dim" href="#0">
+          <Link to={`/motion/${_case.issue_id}`} className="db pv3 ph3 no-underline black dim" href="#0">
                 <div className="flex flex-column flex-row-ns">
                   <div className="pr3 w-40">
                     <img src="http://lorempixel.com/800/600/city" className="db" alt="Motion image thumbnail." />
@@ -24,9 +24,9 @@ export default function MotionCard({_case, totalCount, userVote}) {
                   <div className="w-60 pl3">
                     <h3 className="f5 gray mv0">{_case.number}/{totalCount}</h3>
                     <h1 className="f4 dark-gray mv2 ttu">{_case.title}</h1>
-                    <Link to={`/motion/${_case.issue_id}`} className="f6 link dim br1 ph3 pt2 pb1 mb2 dib white bg-red" href="#0">{userVoteSummary}</Link>
+                    <div className="f6 link dim br1 ph3 pt2 pb1 mb2 dib white bg-red" href="#0">{userVoteSummary}</div>
                   </div>
                 </div>
-              </a>
+              </Link>
             </article>);
 }
