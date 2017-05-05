0c6eb82c49bab63df9be88d536b85754a79e1cc1
Timo Tuominen <tituomin@gmail.com>
2017-05-05 13:35:17 +0300

    Don't show vote link if all votes have been set.

--- src/components/mock/FrontPage.js
+++ src/components/mock/FrontPage.js
@@ -3,6 +3,10 @@ import classNames from 'classnames';
 import {Link} from 'react-router-dom';
 
 export default function FrontPage({nextVote}) {
+  let caseLink = null;
+  if (nextVote) {
+    caseLink = <Link to={`/motion/${nextVote}`} className="f6 link dim br-pill ph3 pv2 mb2 dib white bg-dark-pink mv4" href="#0">Aloitetaan äänestäminen!</Link>;
+  }
   return (<div>
             <article className="center mw7 hidden ba b--light-gray mv4">
                 <div className="pa3 bg-white">
@@ -10,7 +14,7 @@ export default function FrontPage({nextVote}) {
                   <h2 className="f3 gray">Iconic delightful Asia-Pacific Melbourne premium impeccable.</h2>
                   <p className="lh-copy">Monocle ipsum dolor sit amet airport artisanal remarkable international the best Scandinavian. Toto finest sophisticated the best, Boeing 787 delightful cosy tote bag Asia-Pacific uniforms concierge. Extraordinary Winkreative global quality of life artisanal. Vibrant Baggu Boeing 787 alluring signature. Carefully curated izakaya hub elegant. Iconic delightful Asia-Pacific Melbourne premium impeccable. Helsinki charming Boeing 787 Scandinavian, Tsutaya the highest quality global concierge impeccable perfect.</p>
                   <p className="tc">
-                    <Link to={`/motion/${nextVote}`} className="f6 link dim br-pill ph3 pv2 mb2 dib white bg-dark-pink mv4" href="#0">Aloitetaan äänestäminen!</Link>
+                    { caseLink }
                   </p>
                 </div>
             </article>
