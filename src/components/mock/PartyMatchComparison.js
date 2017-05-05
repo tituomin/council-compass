59c9a35a97dcb86742429a2b1f91b158c2d19f10
Timo Tuominen <tituomin@gmail.com>
2017-05-05 15:08:02 +0300

    Party logos.

--- src/components/mock/PartyMatchComparison.js
+++ src/components/mock/PartyMatchComparison.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 
 export default function PartyMatchComparison({agreements}) {
   const vizs = _.map(agreements, (a) => {
-      return <PartyMatchViz key={a.party} partyAbbr={a.party} partyFit={Math.round(((a.agreement + 1)/2.0)*100) }/>;
+      return <PartyMatchViz key={a.party} partyLogo={a.partyLogo} partyAbbr={a.party} partyFit={Math.round(((a.agreement + 1)/2.0)*100) }/>;
   });
   return (<div className="mv4 bg-white pa2 cf tc">
           { vizs }
--- src/components/mock/PartyMatchViz.js
+++ src/components/mock/PartyMatchViz.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 
-export default function PartyMatchViz({partyAbbr, partyFit}) {
+export default function PartyMatchViz({partyAbbr, partyFit, partyLogo}) {
   const indicatorStyle = {
-    backgroundColor: '#006699',
+    backgroundImage: `url(${partyLogo})`,
   };
   return (<div className="dib">
             <a className="link dim white dib br-100 mr1 pv3 w3 h3 ba b--black-10 tc lh-copy" href="#" title=""  style={indicatorStyle}>
