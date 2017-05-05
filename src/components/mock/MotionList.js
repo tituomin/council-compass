0893e10e847f197d1eab525283ddf32ca62cde0e
Tero Tikkanen <tero.tikkanen@gmail.com>
2017-05-05 15:10:51 +0300

    Add user header

--- src/components/mock/MotionList.js
+++ src/components/mock/MotionList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MotionCard from './MotionCard';
+import UserHeader from './UserHeader';
 import _ from 'lodash';
 
 export default function MotionList({cases, userVotes, emptyVotes}) {
@@ -9,9 +10,16 @@ export default function MotionList({cases, userVotes, emptyVotes}) {
   });
   return(
     <div>
-        <div>
-            <a href="#!" onClick={(e) => { emptyVotes(); e.preventDefault(); }}>Tyhjennä äänestyksesi</a>
-        </div>
+      <article className="center bg-white br3 pa3 mv3 ba b--black-10">
+          <div className="tc">
+            <img src="http://tachyons.io/img/avatar_1.jpg" className="br-100 h3 w3 dib" title="Photo of a kitty staring at you" />
+            <h1 className="f4">Omat äänestykset</h1>
+            <hr className="mw3 bb bw1 b--black-10" />
+          </div>
+          <p className="lh-copy tc f6 black-70">
+            <a href="#!" onClick={(e) => { emptyVotes(); e.preventDefault(); }} className="f6 link dim br-pill ph3 pv2 mb2 dib white bg-gray" href="#0">Tyhjennä äänestyksesi</a>
+          </p>
+        </article>
         { cards };
     </div>
   );
