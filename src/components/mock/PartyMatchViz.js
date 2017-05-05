94fbca15db45fc41377c8bb31e81af75779a39c1
Tero Tikkanen <tero.tikkanen@gmail.com>
2017-05-05 18:49:07 +0300

    Fix party logo mouseover text

--- src/components/mock/PartyMatchViz.js
+++ src/components/mock/PartyMatchViz.js
@@ -6,7 +6,7 @@ export default function PartyMatchViz({partyAbbr, partyFit, partyLogo}) {
     backgroundImage: `url(${partyLogo})`,
   };
   return (<div className="dib mb3">
-            <a className="link dim white dib mh2 pv3 w2 h2 tc lh-copy cover bg-center" href="#" title="{partyAbbr}" style={indicatorStyle}>
+            <a className="link dim white dib mh2 pv3 w2 h2 tc lh-copy cover bg-center" href="#" title={partyAbbr} style={indicatorStyle}>
             </a>
             <div className="tc b gray">{partyFit}</div>
           </div>);
