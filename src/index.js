4102caaf7a3463cc50028727038437301a478cbd
Timo Tuominen <tituomin@gmail.com>
2017-05-05 09:41:32 +0300

    Install redux-actions

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import { HashRouter as Router } from 'react-router-dom';
 
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <Router>
-      <App/>
-  </Router>,
+  <Provider store={store}>
+      <Router>
+          <App/>
+      </Router>
+  </Provider>,
   document.getElementById('root'));
+
