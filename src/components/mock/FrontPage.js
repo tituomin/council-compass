6395d4bab4b21218320f2774f6e290392810705c
Juha Yrjölä <juha.yrjola@iki.fi>
2017-05-05 16:32:20 +0300

    More Helsinki

--- src/components/mock/FrontPage.js
+++ src/components/mock/FrontPage.js
@@ -12,7 +12,7 @@ export default function FrontPage({nextVote}) {
                 <div className="pa3 bg-white">
                   <h1 className="f2 tc ttu tracked">Partisipaattori</h1>
                   <hr className="mw3 bb bw1 b--black-10" />
-                  <h2 className="f3 gray tc">Tarkastele ja kerro mielipiteesi kaupunginvaltuuston tehdyistä päätöksistä.</h2>
+                  <h2 className="f3 gray tc">Tarkastele ja kerro mielipiteesi Helsingin kaupunginvaltuuston tehdyistä päätöksistä.</h2>
                   <p className="lh-copy tc">Partisipaattorin avulla voit tarkastella kaupunginvaltuustossa käsiteltyjä asioita ja tarkastella, miten niistä on äänestetty ehdokkaiden, puolueiden ja kaupunkilaisten toimesta. Saat itse kysymystä äänestettyäsi näkyviin omaa kantaasi parhaiten vastaavat edustajat kustakin asiasta. </p>
                   <p className="tc">
                     { caseLink }
