580fbfb9be24b88df43ddf8207083d280933fe0c
Tero Tikkanen <tero.tikkanen@gmail.com>
2017-05-05 12:46:33 +0300

    Add FrontPage component

---
+++ src/components/mock/FrontPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classNames from 'classnames'
+
+export default function FrontPage() {
+  return (<div>
+            <article className="center mw7 hidden ba b--light-gray mv4">
+                <div className="pa3 bg-white">
+                  <h1 className="f2">Palvelun nimi</h1>
+                  <h2 className="f3 gray">Iconic delightful Asia-Pacific Melbourne premium impeccable.</h2>
+                  <p className="lh-copy">Monocle ipsum dolor sit amet airport artisanal remarkable international the best Scandinavian. Toto finest sophisticated the best, Boeing 787 delightful cosy tote bag Asia-Pacific uniforms concierge. Extraordinary Winkreative global quality of life artisanal. Vibrant Baggu Boeing 787 alluring signature. Carefully curated izakaya hub elegant. Iconic delightful Asia-Pacific Melbourne premium impeccable. Helsinki charming Boeing 787 Scandinavian, Tsutaya the highest quality global concierge impeccable perfect.</p>
+                  <p className="tc">
+                    <a className="f6 link dim br-pill ph3 pv2 mb2 dib white bg-dark-pink mv4" href="#0">Aloitetaan äänestäminen!</a>
+                  </p>
+                </div>
+            </article>
+          </div>);
+}
