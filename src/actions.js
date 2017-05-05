a81b334bcaf4090f7069cca70db82b8be8b1020a
Timo Tuominen <tituomin@gmail.com>
2017-05-05 09:55:47 +0300

    Basic redux boilers.

---
+++ src/actions.js
@@ -0,0 +1,3 @@
+import { createAction } from 'redux-actions';
+
+export const castUserVote = createAction('CAST_USER_VOTE');
---
+++ src/reducers.js
@@ -0,0 +1,27 @@
+import { combineReducers } from 'redux';
+import _ from 'lodash';
+
+const initialUserVotes = {
+    votes: [
+    ]
+}
+
+function userVoteReducer(state = initialUserVotes, action) {
+    switch (action.type) {
+    case 'CAST_USER_VOTE':
+      const vote = action.payload;
+      const result = Object.assign(
+        {}, state,
+        { votes: state.votes.concat(vote) });
+      console.log(result);
+      return result;
+    case 'CLEAR_USER_VOTES':
+      return Object.assign(
+        {}, state, {votes: []});
+    }
+  return state;
+}
+
+export default combineReducers({
+   userVotes: userVoteReducer 
+});
