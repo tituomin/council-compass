a0e06c3a46bcf46c67bfa5b8e74fab3ab6583a50
Timo Tuominen <tituomin@gmail.com>
2017-05-05 12:24:38 +0300

    Automatically progressing vote sequence user flow.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Redirect } from 'react-router';
 import AppNavigation from './components/mock/AppNavigation';
 import MotionDetails from './components/mock/MotionDetails';
 import MotionList from './components/mock/MotionList';
@@ -26,17 +27,26 @@ class App extends Component {
     this.cases = cases;
     this.state = {
       initialised: false,
-      userVotes: {}
+      userVotes: {},
+      nextCase: undefined,
+      remainingVotes: this.cases.length
     };
-    window.appState = this.state;
   }
 
   castVote(issueId, value) {
     this.setState(
       (prevState) => {
-        return Object.assign(
+        const userVotes = Object.assign(prevState.userVotes, {[issueId]: value});
+        const nextCases = _.filter(this.cases, (_case) => {
+          return userVotes[_case.issue_id] === undefined;
+        });
+        const nextCaseObject = nextCases[0];
+        this.redirectHandled = false;
+        const nextCase = nextCaseObject ? nextCaseObject.issue_id : undefined;
+        const result = Object.assign(
           {}, prevState,
-          {userVotes: Object.assign(prevState.userVotes, {[issueId]: value})});
+          {userVotes, nextCase, remainingVotes: nextCases.length});
+        return result;
       }
     );
   }
@@ -45,23 +55,30 @@ class App extends Component {
     if (!this.state.initialised) {
       return <div>LOADING...</div>;
     }
+    if (this.state.nextCase && !this.redirectHandled) {
+      this.redirectHandled = true;
+      return <Redirect to={`/motion/${this.state.nextCase}`}/>;
+    }
+    if (this.state.remainingVotes === 0) {
+      return <Redirect to={"/party"}/>;
+    }
     return (
       <div className="App">
         <AppNavigation />
         <div className="App-content bg-washed-green mw7 center cf ph3 pt5">
           <Switch>
+              <Route path="/party" component={PartyList} />
               <Route path="/motion/:id" component={({match}) => {
                     const issueId = match.params.id;
                     const _case = _.find(this.cases, (c) => {return c.issue_id === issueId;});
                     return <MotionDetails _case={_case} castVote={_.bind(this.castVote, this)}/>;
                     }
                   } />
-              <Route path="/" component={
+              <Route path="/motion" component={
                        () => {
                          return <MotionList cases={this.cases} userVotes={this.state.userVotes} />;
                        }
                   } />
-              <Route path="/party" component={PartyList} />
           </Switch>
         </div>
       </div>
